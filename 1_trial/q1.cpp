6ba8cfee99e935f3b9ab64ca4107bce2838e0b9b
alfa934 <alfarras934@gmail.com>
2022-10-24 00:32:56 +0700

    q1.c updated

--- 1_trial/q1.cpp
+++ 1_trial/q1.cpp
@@ -2,6 +2,26 @@
 
 using namespace std;
 
+string input;
+char result[255];
+
+void solve() {
+	getline(cin >> ws, input);
+    int length = input.length();
+
+	int counter = 0;
+	for(int i = 0; i < length; i++) {
+		if(input[i] >= 'a' && input[i] <= 'z') {
+			result[counter] = input[i];
+			counter++;
+		}
+	}
+
+	for(int i = 0; i <counter; i++) {
+			printf("%c", result[i]);
+	}
+}
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -10,22 +30,11 @@ int main() {
 	cin >> t;
 	
 	while(t--) {
-		string input;
-		char result[255];
-		getline(cin >> ws, input);
-        int length = input.length();
-
-        int counter = 0;
-		for(int i = 0; i < length; i++) {
-            if(input[i] >= 'a' && input[i] <= 'z') {
-                result[counter] = input[i];
-                counter++;
-            }
-		}
-		cout << "Case #" << num << ": " << result << endl;
+		cout << "Case #" << num << ": ";
+		solve();
+		printf("\n");
         num++;
 	}
-
 	return 0;
 }
 
@@ -34,7 +43,7 @@ int main() {
 november 22nd 1919
 n$e ve#- rg%&on$ n#ag i've y&o u:up-
 i don't wanna be you anymore
-
+ 
 OUTPUTS
 Case #1: novembernd
 Case #2: nevergonnagiveyouup
