a969159807b5bacb897126262501ffd9172ee266
alfa934 <alfarras934@gmail.com>
2022-10-29 18:13:37 +0700

    add array3d.c

---
+++ 2_curious/array3D.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define MAX_CHAR 50
+#define HEIGHT 5
+#define WIDTH 3
+
+int main() {
+    char arr[HEIGHT][WIDTH][MAX_CHAR] = { // 3D character matrix
+    {"Rat","Bat","Spider"},
+    {"Goblin","Orc","Drawf"},
+    {"Dragon","Lich","Banshee"},
+    {"Demon","Hydra","GiantSpider"},
+    {"Dogs", "Cats", "Horses"}
+ };
+
+    printf("%-17s %-17s %-17s \n", "[NAME]", "[ANIMAL]", "[GENDER]");
+    printf("------------------------------------------------\n");
+    for(int i = 0; i < HEIGHT; i++) {
+        for(int j = 0; j < 3; j++) {
+            printf(" %-17s", arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
