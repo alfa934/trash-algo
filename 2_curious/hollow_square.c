3b1690d5a4f3f26733df56f38542a6d756732dfc
alfa934 <alfarras934@gmail.com>
2022-12-11 04:27:04 +0700

    Update hollow_squre.c

--- 2_curious/hollow_square.c
+++ 2_curious/hollow_square.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+void Sepertator(int width) {
+    for(int i = 0; i < width; i++) {
+        printf("*  ");
+    }
+    printf("\n");
+}
+
 int main() {
     // input whatever you want 👻
     int HEIGHT; 
@@ -9,23 +16,19 @@ int main() {
     printf("Enter Width : "); scanf("%d", &WIDTH);
     printf("\n");
 
-    if(!(HEIGHT < 0 || WIDTH < 0)) {
-
-        for(int i = 0; i < HEIGHT; i++) {
-            for (int j = 0; j < WIDTH; j++) {
-                if(i == 0 || i == HEIGHT - 1) 
-                    printf("*  ");
-                else if(j == 0 || j == WIDTH - 1) 
-                    printf("*  ");
-                else 
-                    printf("   ");
-            }
-            printf("\n");
-        }
-
-    } else {
-        printf("INVALID INPUT\n");
+    if(HEIGHT < 0 || WIDTH < 0) {
+         printf("INVALID INPUT\n");
+         return -1;
     }
 
+    Sepertator(WIDTH);
+    for(int i = 1; i < HEIGHT - 1; i++) {
+        printf("*  ");
+        for (int j = 1; j < WIDTH - 1; j++) {
+            printf("   ");
+        }
+        printf("*\n");
+    }
+    Sepertator(WIDTH);
     return 0;
 }
