88d432122603c01ec5cac3caf8cf3034a81a40c1
alfa934 <99129485+alfa934@users.noreply.github.com>
2022-12-13 06:28:33 +0700

    Update 5_numberFinder.c

--- 3_recruitment/2019/5_numberFinder.c
+++ 3_recruitment/2019/5_numberFinder.c
@@ -3,19 +3,20 @@
 int main() {
 	char string[] = "_41_0V3_14";
 	int counter[10] = {0};
-	int maxNumber = 0;
+	int maxIndex = 0;
 	
 	for(int i = 0; string[i] != '\0'; i++) {
 		if(string[i] >= '0' && string[i] <= '9') {
-			counter[string[i] - 48]++;
-			if(maxNumber < (string[i] - 48)) {
-				maxNumber = string[i] - 48;
+			int index = string[i] - 48;
+			counter[index]++;
+			if(maxIndex < index) {
+				maxIndex = index;
 			}
 		}
 			
 	}
 	
-	for(int i = 0; i <= maxNumber; i++) {
+	for(int i = 0; i <= maxIndex; i++) {
 		printf("%d ", counter[i]);
 	}
 	
